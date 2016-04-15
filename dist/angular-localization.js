260bb21830651b559fdd2b96797d032a969ff7a5
bret.ikehara <bret.ikehara@dailymotion.com>
2016-04-15 01:30:51 -0700

    chore(pkg): release v1.5.0
    update browser language detection and diretive priority.

--- dist/angular-localization.js
+++ dist/angular-localization.js
@@ -1,15 +1,15 @@
 /*!
- * angular-localization :: v1.4.1 :: 2015-12-21
+ * angular-localization :: v1.5.0 :: 2016-04-15
  * web: http://doshprompt.github.io/angular-localization
  *
- * Copyright (c) 2015 | Rahul Doshi
+ * Copyright (c) 2016 | Rahul Doshi
  * License: MIT
  */
 ;(function (angular, window, document, undefined) {
     'use strict';
 
 angular.module('ngLocalize.Version', [])
-    .constant('localeVer', '1.4.1');
+    .constant('localeVer', '1.5.0');
 angular.module('ngLocalize', ['ngSanitize', 'ngLocalize.Config', 'ngLocalize.Events', 'ngLocalize.InstalledLanguages']);
 
 angular.module('ngLocalize.InstalledLanguages', [])
@@ -79,21 +79,43 @@ angular.module('ngLocalize')
             return result;
         }
 
+        function isFrozen (obj) {
+            return (Object.isFrozen || function (obj) {
+                return obj && obj.$$frozen;
+            })(obj);
+        }
+
+        function freeze (obj) {
+            return (Object.freeze || function (obj) {
+                if (obj) {
+                    obj.$$frozen = true;
+                }
+            })(obj);
+        }
+
         function loadBundle(token) {
             var path = token ? token.split('.') : '',
                 root = bundles,
-                url = localeConf.basePath + '/' + currentLocale,
+                parent,
+                locale = currentLocale,
+                url = localeConf.basePath + '/' + locale,
+                ref,
                 i;
 
             if (path.length > 1) {
                 for (i = 0; i < path.length - 1; i++) {
-                    if (!root[path[i]]) {
-                        root[path[i]] = {};
+                    ref = path[i];
+                    if (!root[ref]) {
+                        root[ref] = {};
                     }
-                    root = root[path[i]];
-                    url += '/' + path[i];
+                    parent = root;
+                    root = root[ref];
+                    url += '/' + ref;
                 }
 
+                if (isFrozen(root)) {
+                    root = angular.extend({}, root);
+                }
                 if (!root._loading) {
                     root._loading = true;
 
@@ -112,13 +134,19 @@ angular.module('ngLocalize')
 
                             // Mark the bundle as having been "loaded".
                             delete root._loading;
+                            parent[ref] = freeze(root);
+                            root = null;
 
                             // Notify anyone who cares to know about this event.
-                            $rootScope.$broadcast(localeEvents.resourceUpdates);
+                            $rootScope.$broadcast(localeEvents.resourceUpdates, {
+                                locale: locale,
+                                path: path,
+                                bundle: parent[ref]
+                            });
 
                             // If we issued a Promise for this file, resolve it now.
                             if (deferrences[path]) {
-                                deferrences[path].resolve(data);
+                                deferrences[path].resolve(path);
                             }
                         })
                         .error(function (err) {
@@ -152,7 +180,7 @@ angular.module('ngLocalize')
             }
 
             if (bundle && !bundle._loading) {
-                deferrences[path].resolve(bundle);
+                deferrences[path].resolve(path);
             } else {
                 if (!bundle) {
                     loadBundle(token);
@@ -261,19 +289,31 @@ angular.module('ngLocalize')
             $html.attr('lang', lang);
         }
 
+        function getLanguageSupported(language) {
+            var foundLanguage = null;
+            if (language && language.length) {
+                localeSupported.forEach(function (languageSuppported) {
+                    if (languageSuppported.indexOf(language) === 0) {
+                        foundLanguage = languageSuppported;
+                        return;
+                    }
+                });
+                if (!foundLanguage) {
+                    var fallbackLang = localeFallbacks[language.split('-')[0]];
+                    if (!angular.isUndefined(fallbackLang)) {
+                      foundLanguage = fallbackLang;
+                    }
+                }
+            }
+            return foundLanguage || localeConf.defaultLocale;
+        }
+
         function setLocale(value) {
             var lang;
 
-            if (angular.isString(value)) {
+            if (angular.isString(value) && value.length ) {
                 value = value.trim();
-                if (localeSupported.indexOf(value) !== -1) {
-                    lang = value;
-                } else {
-                    lang = localeFallbacks[value.split('-')[0]];
-                    if (angular.isUndefined(lang)) {
-                        lang = localeConf.defaultLocale;
-                    }
-                }
+                lang = getLanguageSupported(value);
             } else {
                 lang = localeConf.defaultLocale;
             }
@@ -286,7 +326,6 @@ angular.module('ngLocalize')
                 updateHtmlTagLangAttr(lang);
 
                 $rootScope.$broadcast(localeEvents.localeChanges, currentLocale);
-                $rootScope.$broadcast(localeEvents.resourceUpdates);
 
                 if (cookieStore) {
                     cookieStore.put(localeConf.cookieName, lang);
@@ -298,7 +337,39 @@ angular.module('ngLocalize')
             return currentLocale;
         }
 
-        setLocale(cookieStore && cookieStore.get(localeConf.cookieName) ? cookieStore.get(localeConf.cookieName) : $window.navigator.userLanguage || $window.navigator.language);
+        function getPreferredBrowserLanguage() {
+            var nav = $window.navigator,
+                browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
+                i,
+                language;
+
+            // support for HTML 5.1 "navigator.languages"
+            if (angular.isArray(nav.languages)) {
+                for (i = 0; i < nav.languages.length; i++) {
+                    language = nav.languages[i];
+                    if (language) {
+                        return language;
+                    }
+                }
+            }
+            // support for other well known properties in browsers
+            for (i = 0; i < browserLanguagePropertyKeys.length; i++) {
+                language = nav[browserLanguagePropertyKeys[i]];
+                if (language) {
+                    return language;
+                }
+            }
+
+            return null;
+        }
+
+        function initialSetLocale() {
+            setLocale(cookieStore && cookieStore.get(localeConf.cookieName) ?
+                cookieStore.get(localeConf.cookieName) :
+                getPreferredBrowserLanguage());
+        }
+
+        initialSetLocale();
 
         return {
             ready: ready,
@@ -307,7 +378,8 @@ angular.module('ngLocalize')
             getKey: getKey,
             setLocale: setLocale,
             getLocale: getLocale,
-            getString: getLocalizedString
+            getString: getLocalizedString,
+            getPreferredBrowserLanguage: getPreferredBrowserLanguage
         };
     }]);
 
@@ -350,7 +422,7 @@ angular.module('ngLocalize')
 
             attrs.$observe('i18n', function (newVal, oldVal) {
                 if (newVal && newVal !== oldVal) {
-                    update(elm, newVal, hasObservers); 
+                    update(elm, newVal, hasObservers);
                 }
             });
 
@@ -374,12 +446,15 @@ angular.module('ngLocalize')
             });
         };
     }])
-    .directive('i18nAttr', ['locale', 'localeEvents', function (locale, localeEvents) {
-        return function (scope, elem, attrs) {
-            var lastValues = {};
+    .directive('i18nAttr', ['$rootScope', 'locale', 'localeEvents', function ($rootScope, locale, localeEvents) {
+        function setAttr ($attrs, key, value) {
+            $attrs.$set($attrs.$normalize(key), value);
+        }
 
-            function updateText(target, attributes) {
-                var values = scope.$eval(attributes),
+        function getUpdateText ($scope, target, $attrs) {
+            var lastValues = {};
+            return function (attributes) {
+                var values = $scope.$eval(attributes),
                     langFiles = [],
                     exp;
 
@@ -392,29 +467,46 @@ angular.module('ngLocalize')
 
                 locale.ready(langFiles).then(function () {
                     var value = '';
-
                     for(var key in values) {
                         exp = values[key];
                         value = locale.getString(exp);
                         if (lastValues[key] !== value) {
-                            attrs.$set(attrs.$normalize(key), lastValues[key] = value);
+                            lastValues[key] = value;
+                            setAttr($attrs, key, value);
                         }
                     }
                 });
+            };
+        }
+
+        return {
+            // ensure has higher priority than ngAria
+            priority: 1000,
+            compile: function ($elem, $attrs) {
+                angular.forEach($rootScope.$eval($attrs.i18nAttr), function (value, key) {
+                    // temporarily populate attribute
+                    // avoid false positive warning about aria-label
+                    setAttr($attrs, key, value || '...');
+                });
+
+                return function ($scope, $elem, $attrs) {
+                    var updateText = getUpdateText($scope, $elem, $attrs);
+
+                    $attrs.$observe('i18nAttr', function (newVal) {
+                        if (newVal) {
+                            updateText(newVal);
+                        }
+                    });
+
+                    $scope.$on(localeEvents.resourceUpdates, function () {
+                        updateText($attrs.i18nAttr);
+                    });
+
+                    $scope.$on(localeEvents.localeChanges, function () {
+                        updateText($attrs.i18nAttr);
+                    });
+                };
             }
-
-            attrs.$observe('i18nAttr', function (newVal) {
-                if (newVal) {
-                    updateText(elem, newVal); 
-                }
-            });
-
-            scope.$on(localeEvents.resourceUpdates, function () {
-                updateText(elem, attrs.i18nAttr);
-            });
-            scope.$on(localeEvents.localeChanges, function () {
-                updateText(elem, attrs.i18nAttr);
-            });
         };
     }]);
 
