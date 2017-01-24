d9dd54c80f830181a9e10e8fb8ac8b11564c1edc
U-ACTIMIND\gorbach <gorbach@W7GORBACH.ACTIMIND>
2017-01-24 18:15:11 +0300

    lint fix

--- src/localization.urlBuilder.js
+++ src/localization.urlBuilder.js
@@ -1,14 +1,12 @@
 angular.module('ngLocalize.UrlBuilder', [])
     .value('urlBuilder', { 
 		build: function(basePath, locale, refs, fileExtension) {
-			if (!angular.isArray(refs))
+			if (!angular.isArray(refs)) {
 				refs = [refs];
+			}
 			
 			var delimiter = '/';
 			
-			return basePath 
-			+ delimiter + locale 
-			+ delimiter + refs.join(delimiter)
-			+ fileExtension;
+			return basePath + delimiter + locale + delimiter + refs.join(delimiter) + fileExtension;
 		}
 	});
